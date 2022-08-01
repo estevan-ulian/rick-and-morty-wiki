0738904fcad548d0a9cc526cd9a351370fd1ad02
Estevan Ulian <estevan.ulian@gmail.com>
2022-08-01 08:51:42 -0300

    Install React Icons and update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
+import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { AiFillCloseCircle, AiFillHeart, AiFillMinusCircle, AiOutlineReload, AiOutlineSearch } from 'react-icons/ai'
 
 const publicEndpoint = 'https://rickandmortyapi.com/api/character';
-
-export async function getStaticProps() {
-  const res = await fetch(publicEndpoint);
-  const data = await res.json();  
-  
-  return {
-    props: {
-      data
-    },
-    revalidate: 10,
-  };
-}
 
 export default function Home({ data }) {
   const { info, results: defaultResults = [] } = data;
@@ -71,32 +62,77 @@ export default function Home({ data }) {
       // @ts-ignore
       const value = fieldQuery.value || '';
       const endPoint = `https://rickandmortyapi.com/api/character/?name=${value}`
-      setPage({
-        current: endPoint
-      });
+      if(value) {
+        return setPage({
+          current: endPoint
+        });
+      } else {
+        return (
+          <div>
+            nao funcionou
+          </div>
+        )
+      }      
     }
 
 
   return (
     <>
-      <form onSubmit={handleOnSubmitSearch}>
-        <input type="search" name="query" />
-        <button>Pesquisar</button>
+    <Head>
+    <title>Rick and Mory - Wiki Brasil</title>
+    </Head>
+    <div className="py-4 flex justify-center bg-slate-500">
+      <h1 className="text-white text-2xl">Rick and Morty - Wiki Brasil</h1>
+    </div>
+    <div className="h-28 flex justify-center items-center px-4">
+      <form onSubmit={handleOnSubmitSearch} className="flex gap-2 w-full justify-center">
+        <input type="search" placeholder="Pesquisar personagem..." name="query" className="w-full sm:w-2/5" />
+        <button className="w-8 flex justify-center items-center hover:bg-gray-200"><AiOutlineSearch className="w-5 h-5" /></button>
       </form>
-      <div className="flex flex-wrap gap-2 w-full mx-auto">        
+    </div>
+
+      <div className="flex flex-wrap gap-4 justify-center w-full mx-auto">        
         {
           results.map(item => {
             return (
-              <article key={item.id} className={`min-w-max`}>
-                <Image src={item.image} width={300} height={300} alt={item.name} />
-                <h3 className="font-bold">{item.name}</h3>
-              </article>
+              <div key={item.id} className="border border-slate-200 transition-all duration-300 hover:scale-105">
+                <Link href={`/character/${item.id}`}>
+                  <a>
+                    <article className={`w-full sm:w-72`}>
+                      <Image src={item.image} width={300} height={300} alt={item.name} />
+                      <div className="p-2 flex flex-col gap-2">                      
+                        <div className="flex justify-between items-center flex-wrap">
+                          <h3 className="font-bold">{item.name}</h3>
+                          <span>{item.status === 'Alive' ? <AiFillHeart className="fill-green-500" /> : <AiFillCloseCircle className="fill-red-500" />}</span>
+                        </div>
+                        <div className="flex justify-between flex-wrap">
+                          <span>Espécie: {item.species === 'unknown' ? 'Desconhecida' : item.species}</span>
+                          <span>Gênero: {item.gender === 'unknown' ? 'Desconhecido' : item.gender}</span>
+                        </div>
+                      </div>
+                    </article>
+                  </a>
+                </Link>
+              </div>
             )
           })
         }
       </div>
-        <button onClick={() => handleLoadMore()}>Load More</button>
-
+      <div className="flex items-center justify-center py-4 mt-4">
+        <button onClick={() => handleLoadMore()} className="flex items-center justify-center gap-2 border px-4 py-2 border-slate-300 transition-all duration-300 hover:scale-105">+ Carregar mais personagens</button>
+      </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(publicEndpoint);
+  const data = await res.json();  
+  
+  return {
+    props: {
+      data
+    },
+    revalidate: 10,
+  };
 }
