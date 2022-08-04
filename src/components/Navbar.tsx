6972a07d78ae74f71c3d85057072bc7d7a145cca
Estevan Ulian <estevan.ulian@gmail.com>
2022-08-04 16:08:25 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Container from "./Container";
 import ToggleMenu from "./ToggleMenu";
 
@@ -33,17 +32,12 @@ export default function Navbar() {
                         </Link>
                     </div>
                     <ToggleMenu open={open} onClick={() => handleToggleMenu()} />
-                    <nav className={`
-                        lg:w-4/5 absolute lg:static top-24 px-2 z-10 w-full h-[calc(100vh-96px)] lg:h-auto
-                        flex flex-col lg:flex-row gap-10 lg:gap-2 items-center justify-center bg-gradient-to-r from-slate-900 to-slate-800 lg:bg-gradient-to-r lg:from-transparent lg:to-transparent
-                        transition-all duration-500 ease-in-out ${open ? 'right-0' : 'right-[-100%]'}
-                    `}
-                    >
+                    <nav className={`lg:w-4/5 absolute lg:static top-24 px-2 z-10 w-full h-[calc(100vh-96px)] lg:h-auto flex flex-col lg:flex-row gap-10 lg:gap-2 items-center justify-center bg-gradient-to-r from-slate-900 to-slate-800 lg:bg-gradient-to-r lg:from-transparent lg:to-transparent transition-all duration-300 ease-in-out ${open ? 'left-0' : 'left-[-100%]'}`}>
                         <ul className="w-full flex flex-col lg:flex-row items-center justify-end gap-10 lg:gap-3">
-                            <li><Link href="/"><a className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Personagens</a></Link></li>
-                            <li><Link href="/episode"><a className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Episódios</a></Link></li>
-                            <li><Link href="/location"><a className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Localidades</a></Link></li>
-                            <li><Link href="/creditos"><a className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Créditos</a></Link></li>
+                            <li><Link href="/"><a onClick={() => setOpen(false)} className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Personagens</a></Link></li>
+                            <li><Link href="/episode"><a onClick={() => setOpen(false)} className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Episódios</a></Link></li>
+                            <li><Link href="/location"><a onClick={() => setOpen(false)} className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Localidades</a></Link></li>
+                            <li><Link href="/creditos"><a onClick={() => setOpen(false)} className="text-4xl lg:text-base font-light text-white lg:font-bold px-3 py-2 border-b border-transparent transition-all duration-300 hover:border-b-2 hover:border-[#b2df28]">Créditos</a></Link></li>
                         </ul>
                     </nav>
 
