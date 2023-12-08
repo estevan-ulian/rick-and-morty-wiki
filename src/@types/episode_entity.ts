c2f6d8a402c82e7cd256e8d8d334b08791815e31
Estevan Ulian <estevan.ulian@gmail.com>
2023-12-08 07:57:33 -0300

    Created entities

---
+++ src/@types/episode_entity.ts
@@ -0,0 +1,11 @@
+export type TEpisode = {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+};
+
+// filters => name(string), episode(code)
